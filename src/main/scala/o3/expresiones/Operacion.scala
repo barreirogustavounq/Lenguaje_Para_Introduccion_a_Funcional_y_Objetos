9a938ee939d2febdea24d77812f4f9624bc18d9b
SGuille <guillesalvatore@gmail.com>
2020-06-21 11:28:02 -0300

    Corrección

--- src/main/scala/o3/expresiones/Operacion.scala
+++ src/main/scala/o3/expresiones/Operacion.scala
@@ -1,18 +1,18 @@
 package o3.expresiones
 
-case class Suma(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Resta(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Multiplicacion(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Division(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Mayor(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Menor(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Igual(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class Distinto(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class MenorOIgual(n1: Numero, n2: Numero) extends Operacion(n1,n2)
-case class MayorOIgual(n1: Numero, n2: Numero) extends Operacion(n1,n2)
+case class Suma(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Resta(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Multiplicacion(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Division(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Mayor(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Menor(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Igual(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class Distinto(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class MenorOIgual(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
+case class MayorOIgual(n1: Expresion, n2: Expresion) extends Operacion(n1,n2)
 
 /**********OPERACION DESCONOCIDA**************/
 
-class Operacion(n1: Numero, n2: Numero) extends Expresion
+class Operacion(n1: Expresion, n2: Expresion) extends Expresion
 
 
