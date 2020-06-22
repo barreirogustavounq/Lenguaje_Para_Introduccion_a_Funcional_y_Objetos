7007c38af8f0955fa730600630f39ea7544094d1
SGuille <guillesalvatore@gmail.com>
2020-06-21 21:27:42 -0300

    Refactor optimizador

--- src/main/scala/o3/motores/Optimizador.scala
+++ src/main/scala/o3/motores/Optimizador.scala
@@ -7,15 +7,7 @@ import o3.reglamento.Reglamento
 class Optimizador() {
   def optimizarOperacion(expresion: Expresion): Expresion = {
     val r = Reglamento.relgaPara(expresion)
-    var optimizacion : List[Expresion] = List()
-    var resultado = expresion
-    optimizacion = optimizacion.appended(r.optimizar(expresion))
-    optimizacion.foreach{e =>
-      if(e != expresion){
-        resultado = e
-      }
-    }
-    resultado
+    r.optimizar(expresion)
   }
   def optimizarPrograma(programa: Programa): Unit ={
     val optimizacion = programa.elementos.map(e => optimizarOperacion(e))
